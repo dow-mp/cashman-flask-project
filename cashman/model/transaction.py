07aa7ea2e2a6c8459f57bbf4aef9c69a0d3f5f0b
dow-mp <derin.mitchell@gmail.com>
2025-03-08 13:15:15 -0500

    implement transaction ids

--- cashman/model/transaction.py
+++ cashman/model/transaction.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import uuid
 
 from marshmallow import Schema, fields # install with uv using uv add marshmallow
 # marshmallow is a package for converting complex datatypes to and from built-in Python datatypes; we'll use it to validate, serialize, and deserialize data
@@ -10,12 +11,14 @@ class Transaction(object):
         self.amount = amount
         self.created_at = dt.datetime.now()
         self.type = type
+        self.id = uuid.uuid4()
 
     def __repr__(self):
-        return f"<Transaction(name={self.description!r})>".format(self=self)
+        return f"<Transaction(name={self.description!r}, id={self.id!r})>".format(self=self)
     
 # used to deserialize and serialize instances of Transaction from and to JSON objects
 class TransactionSchema(Schema):
+    id = fields.Str()
     description = fields.Str()
     amount = fields.Number()
     created_at = fields.Date()
